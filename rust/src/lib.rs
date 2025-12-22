05e342cff0a044d128af23f5cdbedc165a9d9ad9
weirui <weirui@wps.cn>
2025-12-22 13:03:32 +0800

    移除rust

--- rust/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod im;
-
-// 重新导出常用类型和函数，方便外部使用
-pub use im::{
-    client::{ClientConfig, OpenIMClient},
-    conversation::{ConversationSyncer, ConversationSyncerConfig},
-    friend::LocalFriend,
-    login_async, AllConversationsResp, IncrementalConversationResp, LocalConversation,
-};
